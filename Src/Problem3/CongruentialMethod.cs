d87888a52ab7ece9b15173489b33b87aa9c9e0aa
Mohammadreza <Live@taesiri.com>
2012-11-24 11:03:24 +0330

    Problem 3 Added

---
+++ Src/Problem3/CongruentialMethod.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Problem3
+{
+    public static class CongruentialMethod
+    {
+        public static List<float> MixedCongruential(int modulus, int seed, int multiplier, int increment)
+        {
+            var randomNumbers = new List<float> ();
+
+            long numb = (multiplier * seed) + increment;
+            randomNumbers.Add(((numb) % modulus) / (float)modulus);
+
+            do
+            {
+                numb = (multiplier*numb) + increment;
+                randomNumbers.Add(((numb)%modulus)/(float) modulus);
+
+            } while (randomNumbers[0] != randomNumbers[randomNumbers.Count - 1]);
+
+
+            return randomNumbers;
+        }
+        public static List<float> MultiplicativeCongruential(int modulus, int seed, int multiplier)
+        {
+
+            return new List<float>();
+        }
+    }
+}
---
+++ Src/Problem3/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Problem3.RandomGenerator;
+
+namespace Problem3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var home = new Home();
+            home.ShowDialog();
+            List<float> test = CongruentialMethod.MixedCongruential(100, 27, 4, 6);
+            test.ToString();
+        }
+    }
+}
